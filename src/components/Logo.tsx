20680ec11fa79071d4c9962c4c8ddfabfe29bdfa
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-03-21 18:43:14 +0200

    Replacing logo formats

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ const Logo = ({
   return (
     <div className={className}>
       <Link href="/" className="flex items-center gap-2">
-        <Image src="/logo.png" alt="Yuppi Logo" width={32} height={32} />
+        <Image src="/logo.svg" alt="Yuppi Logo" width={32} height={32} />
         <span className="text-2xl">Yuppi</span>
       </Link>
     </div>
