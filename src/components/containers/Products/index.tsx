0726a587ab81f1f6567ce78187e33b93da78d636
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-06 02:09:46 +0300

    * Improved product card and added product data

--- src/components/containers/Products/index.tsx
+++ src/components/containers/Products/index.tsx
@@ -4,50 +4,29 @@ import SectionContainer from '@/layouts/SectionContainer';
 import ProductCard from '@/components/containers/Products/ProductCard';
 import GitHub from '@/components/icons/social/GitHub';
 
+import { PRODUCTS_DATA as getProducts } from '@/data/getProductsCard';
+
 const Products: FC = () => {
   return (
     <SectionContainer className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-4 xl:gap-3 2xl:gap-6 md:grid-cols-12'>
-      <ProductCard
-        className="col-span-12 md:col-span-8 xl:col-span-6"
-        alignLeft
-        url="/"
-        title="Yuppi"
-        subtitle="An easy-to-use React library for building forms and data validation"
-        icon={<GitHub />}
-      />
-
-      <ProductCard
-        className="col-span-12 md:col-span-4 xl:col-span-3"
-        url="/"
-        title="Yuppi"
-        subtitle="An easy-to-use React library for building forms and data validation"
-        icon={<GitHub />}
-      />
-
-      <ProductCard
-        className="col-span-12 xl:col-span-3"
-        url="/"
-        title="Yuppi"
-        subtitle="An easy-to-use React library for building forms and data validation"
-        icon={<GitHub />}
-      />
-
-      <ProductCard
-        className="col-span-12 md:col-span-4  xl:col-span-3"
-        url="/"
-        title="Yuppi"
-        subtitle="An easy-to-use React library for building forms and data validation"
-        icon={<GitHub />}
-      />
-
-      <ProductCard
-        className="col-span-12 md:col-span-8  xl:col-span-9"
-        alignLeft
-        url="/"
-        title="Yuppi"
-        subtitle="Enhance your programming abilities by engaging in a variety of bite-sized exercises designed to test and improve your proficiency."
-        icon={<GitHub />}
-      />
+      {getProducts.map((data, index) => (
+        <ProductCard
+          key={data.label}
+          className={
+            index === 0 ? 'col-span-12 md:col-span-8 xl:col-span-6' :
+              index === 1 ? 'col-span-12 md:col-span-4 xl:col-span-3' :
+                index === 2 ? 'col-span-12 xl:col-span-3' :
+                  index === 3 ? 'col-span-12 md:col-span-4 xl:col-span-3' :
+                    index === 4 ? 'col-span-12 md:col-span-8  xl:col-span-9' :
+                      'col-span-12 md:col-span-4 xl:col-span-3'
+          }
+          alignLeft={index === 0 || index === 4}
+          url={data.product.url}
+          title={data.product.title}
+          subtitle={data.product.subtitle}
+          icon={<GitHub />}
+        />
+      ))}
     </SectionContainer>
   );
 };
---
+++ src/data/getProductsCard.ts
@@ -0,0 +1,42 @@
+export const PRODUCTS_DATA = [
+  {
+    label: "first",
+    product: {
+      title: "Yuppi",
+      subtitle: "An easy-to-use React library for building forms and data validation",
+      url: "/",
+    },
+  },
+  {
+    label: "second",
+    product: {
+      title: "Yuppi",
+      subtitle: "An easy-to-use React library for building forms and data validation",
+      url: "/",
+    },
+  },
+  {
+    label: "third",
+    product: {
+      title: "Yuppi",
+      subtitle: "An easy-to-use React library for building forms and data validation",
+      url: "/",
+    },
+  },
+  {
+    label: "fourth",
+    product: {
+      title: "Yuppi",
+      subtitle: "An easy-to-use React library for building forms and data validation",
+      url: "/",
+    },
+  },
+  {
+    label: "fifth",
+    product: {
+      title: "Yuppi",
+      subtitle: "Enhance your programming abilities by engaging in a variety of bite-sized exercises designed to test and improve your proficiency.",
+      url: "/",
+    },
+  },
+];
