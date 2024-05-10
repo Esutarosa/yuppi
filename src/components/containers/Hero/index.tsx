ac0d06415ea22fa806cde8bc2844ee979eb829c6
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-10 22:27:11 +0300

    > Fixed typo

--- src/components/containers/Hero/index.tsx
+++ src/components/containers/Hero/index.tsx
@@ -13,7 +13,7 @@ const Hero: FC = () => {
           <div className='relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8'>
             <div className='flex flex-col items-center'>
               <h1 className='text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl'>Yuppi Quizzes</h1>
-              <p className='text-muted-foreground my-3 pt-2 text-sm sm:mt-5 lg:mb-0 sm:text-base lg:text-lg'>Let's test your preparation and mental acuity with our quizzes, problems and tasks on frontend and related programming languages.</p>
+              <p className='text-muted-foreground my-3 pt-2 text-sm sm:mt-5 lg:mb-0 sm:text-base lg:text-lg'>Let's test your preparation and mental acuity with our quizzes on frontend and related programming languages.</p>
             </div>
             <div className='flex items-center gap-2'>
               <Button asChild variant="default" className="bg-primary/75 text-foreground">
