9be3b73df2cbf176d1330f8da448b4d424bda819
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-13 10:03:46 +0300

    + Added starting structure for the quiz

---
+++ src/components/containers/Quiz/QuizCard/index.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const QuizCard: FC = () => {
+  return (
+    <div>
+      index
+    </div>
+  );
+};
+
+export default QuizCard;
--- src/components/containers/Quiz/index.tsx
+++ src/components/containers/Quiz/index.tsx
@@ -1,10 +1,49 @@
+'use client';
+
 import type { FC } from 'react';
+import { useState } from "react";
+
+import SectionContainer from '@/layouts/SectionContainer';
+import QuizCard from '@/components/containers/Quiz/QuizCard';
+import { Button } from '@/components/ui/button';
 
 const Quiz: FC = () => {
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [name, setName] = useState('');
+
   return (
-    <div>
-      Quiz
-    </div>
+    <SectionContainer className='mt-16 flex flex-col justify-center items-center gap-4'>
+      <div className='text-center'>
+        <h1>Quiz</h1>
+      </div>
+
+      {quizStarted ? (
+        <QuizCard />
+      ) : (
+        <>
+          <div className="flex flex-col gap-2 mb-6">
+            <label htmlFor="nameInput" className="text-foreground">
+              Enter your name:
+            </label>
+            <input
+              type="text"
+              className="max-w-lg bg-[inherit] border border-border p-2 text-foreground"
+              id="nameInput"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <Button
+            variant="default"
+            onClick={() => setQuizStarted(true)}
+            disabled={!name.trim()}
+          >
+            Start Quiz
+          </Button>
+        </>
+      )}
+    </SectionContainer>
   );
 };
 
